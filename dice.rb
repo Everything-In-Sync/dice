5a7e7ebac041a01b84e346fc8ba2ff179601ea68
Rob Kososki <38138055+robkos442@users.noreply.github.com>
2018-04-19 19:54:56 -0400

    Add files via upload
    Generates a random number between 1-12
    Loops the whole program

--- dice.rb
+++ dice.rb
@@ -1,13 +1,16 @@
+loop do #loops the entire program
 puts "Welcome to the dice rolling program"
-puts "Would you like to roll the dice? y/n"
-answer = gets.chomp!
-if answer.downcase == "y"
+puts "Would you like to roll the dice? y/n" #asks for yes or no input
+answer = gets.chomp! #gets input
+if answer.downcase == "y" #makes the input lower case
   puts "Great! Let's get rolling!"
   puts "Your number is..."
 my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-print my_array.sample(1)
+print my_array.sample(1) #prints random integer from array
 elsif answer.downcase == "n"
   puts "Maybe next time!"
 else
   puts "I do not understand your command."
 end
+end
+
